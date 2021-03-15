abf3ce69c310d62821c25647233f857258fbf9bf
Patrick Ataíde <43658340+pasilva1@users.noreply.github.com>
2021-03-15 09:44:03 -0300

    Add files via upload

---
+++ NETFLIX/js/owl/setup.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:8,
+    nav:true,
+    autoplay: true,
+    autoplayTimeout:800,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:5
+        },
+        600:{
+            items:7
+        },
+        1000:{
+            items:10
+        }
+    }
+})
+
